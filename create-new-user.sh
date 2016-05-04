e0fea1ba4a50009e8cf01cb2b4f674bc4ecc0e1e
Stijn Ruts <github@stijnruts.be>
2016-05-04 21:51:29 +0200

    Cleanup add new user

--- create-new-user.sh
+++ create-new-user.sh
@@ -1,87 +1,43 @@
 #!/usr/bin/env bash
 
-add_user_to_group() {
-    local _user=${1}
-    local _group=${2}
+# Loop while user name is blank, has spaces, or has capital letters in it.
+USER=""
+while [[ ${#USER} -eq 0 ]] || [[ $USER =~ \ |\' ]] || [[ $USER =~ [^a-z0-9\ ] ]]; do
+    read -p "username:" USER
+done
 
-    if [[ -z ${_group} ]]; then
-        error_msg "ERROR! 'add_user_to_group' was not given enough parameters."
-    fi
+# Add the user
+# -m creates the user's home directory
+# -g the user's initial login group
+# -G a list of supplementary groups
+# -s defines the user's default login shell
+useradd ${USER} -m -g users -G storage,power,network,video,audio,lp -s /bin/bash
 
-    ncecho " ${BBlue}[${Reset}${Bold}X${BBlue}]${Reset} Adding ${Bold}${_user}${Reset} to ${Bold}${_group}${Reset} "
-    groupadd ${_group} >>"$LOG" 2>&1 &
-    gpasswd -a ${_user} ${_group} >>"$LOG" 2>&1 &
-    pid=$!;progress $pid
-}
+# Prompt for password
+echo "password:"
+passwd $USER
 
-# Originally adapted from the Antergos 2.0 installer
-create_new_user() {
+# Setup sudo access
+read -r -p "Setup sudo for $USER? [y/N] " response
+if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    package_install "sudo"
+    # set sudo access for the 'wheel' group in /etc/sudoers.d/
+    # instead of editing /etc/sudoers directly
+    echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers.d/wheel
+    # Add user to the 'wheel' group
+    groupadd wheel
+    gpasswd -a ${USER} wheel
+fi
 
-    dialog --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " $_NUsrTitle " --inputbox "$_NUsrBody" 0 0 "" 2>${ANSWER} || config_base_menu
-    USER=$(cat ${ANSWER})
+# Set up basic configuration files and permissions for user
+cp /etc/skel/.bashrc /home/${USER}
+chown -R ${USER}:users /home/${USER}
 
-    # Loop while user name is blank, has spaces, or has capital letters in it.
-    while [[ ${#USER} -eq 0 ]] || [[ $USER =~ \ |\' ]] || [[ $USER =~ [^a-z0-9\ ] ]]; do
-        dialog --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " $_NUsrTitle " --inputbox "$_NUsrErrBody" 0 0 "" 2>${ANSWER} || config_base_menu
-        USER=$(cat ${ANSWER})
-    done
+# Create user dirs
+package_install "xdg-user-dirs"
+su - ${USER} -c "xdg-user-dirs-update"
 
-    # Enter password. This step will only be reached where the loop has been skipped or broken.
-    dialog --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " $_ConfUsrNew " --clear --insecure --passwordbox "$_PassNUsrBody $USER\n\n" 0 0 2> ${ANSWER} || config_base_menu
-    PASSWD=$(cat ${ANSWER})
-
-    dialog --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " $_ConfUsrNew " --clear --insecure --passwordbox "$_PassReEntBody" 0 0 2> ${ANSWER} || config_base_menu
-    PASSWD2=$(cat ${ANSWER})
-
-    # loop while passwords entered do not match.
-    while [[ $PASSWD != $PASSWD2 ]]; do
-        dialog --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " $_ErrTitle " --msgbox "$_PassErrBody" 0 0
-
-        dialog --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " $_ConfUsrNew " --clear --insecure --passwordbox "$_PassNUsrBody $USER\n\n" 0 0 2> ${ANSWER} || config_base_menu
-        PASSWD=$(cat ${ANSWER})
-
-        dialog --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " $_ConfUsrNew " --clear --insecure --passwordbox "$_PassReEntBody" 0 0 2> ${ANSWER} || config_base_menu
-        PASSWD2=$(cat ${ANSWER})
-    done
-
-    # create new user. This step will only be reached where the password loop has been skipped or broken.
-    dialog --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " $_ConfUsrNew " --infobox "$_NUsrSetBody" 0 0
-    sleep 2
-    # Create the user, set password, then remove temporary password file
-    arch_chroot "useradd ${USER} -m -g users -G wheel,storage,power,network,video,audio,lp -s /bin/bash" 2>/tmp/.errlog
-    check_for_error
-    echo -e "${PASSWD}\n${PASSWD}" > /tmp/.passwd
-    arch_chroot "passwd ${USER}" < /tmp/.passwd >/dev/null 2>/tmp/.errlog
-    rm /tmp/.passwd
-    check_for_error
-    # Set up basic configuration files and permissions for user
-    arch_chroot "cp /etc/skel/.bashrc /home/${USER}"
-    arch_chroot "chown -R ${USER}:users /home/${USER}"
-    [[ -e ${MOUNTPOINT}/etc/sudoers ]] && sed -i '/%wheel ALL=(ALL) ALL/s/^#//' ${MOUNTPOINT}/etc/sudoers
-
-}
-
-configure_sudo() {
-    if ! is_package_installed "sudo" ; then
-        print_title "SUDO - https://wiki.archlinux.org/index.php/Sudo"
-        package_install "sudo"
-    fi
-    if [[ ! -f  /etc/sudoers.aui ]]; then
-        cp -v /etc/sudoers /etc/sudoers.aui
-        ## Uncomment to allow members of group wheel to execute any command
-        sed -i '/%wheel ALL=(ALL) ALL/s/^#//' /etc/sudoers
-        ## Same thing without a password (not secure)
-        #sed -i '/%wheel ALL=(ALL) NOPASSWD: ALL/s/^#//' /etc/sudoers
-
-        #This config is especially helpful for those using terminal multiplexers like screen, tmux, or ratpoison, and those using sudo from scripts/cronjobs:
-        echo "" >> /etc/sudoers
-        echo 'Defaults !requiretty, !tty_tickets, !umask' >> /etc/sudoers
-        echo 'Defaults visiblepw, path_info, insults, lecture=always' >> /etc/sudoers
-        echo 'Defaults loglinelen=0, logfile =/var/log/sudo.log, log_year, log_host, syslog=auth' >> /etc/sudoers
-        echo 'Defaults passwd_tries=3, passwd_timeout=1' >> /etc/sudoers
-        echo 'Defaults env_reset, always_set_home, set_home, set_logname' >> /etc/sudoers
-        echo 'Defaults !env_editor, editor="/usr/bin/vim:/usr/bin/vi:/usr/bin/nano"' >> /etc/sudoers
-        echo 'Defaults timestamp_timeout=15' >> /etc/sudoers
-        echo 'Defaults passprompt="[sudo] password for %u: "' >> /etc/sudoers
-    fi
-}
+# Passing aliases
+# If you use a lot of aliases, you might have noticed that they do not carry over to the root account when using sudo.
+# However, there is an easy way to make them work. Simply add the following to your ~/.bashrc or /etc/bash.bashrc:
+# alias sudo='sudo '
--- install-aur-helper.sh
+++ install-aur-helper.sh
@@ -1,1 +1,7 @@
 #!/usr/bin/env bash
+
+# ensure that the necessary tools are installed
+sudo pacman -S --needed base-devel
+
+# install yaourt (and package-query dependency) though the aur.sh helper
+bash <(curl aur.sh) -si package-query yaourt
