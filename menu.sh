6f197e413d7e538e580d5574e11fd0069285495a
Stijn Ruts <github@stijnruts.be>
2016-05-04 16:30:19 +0200

    Cleanup main menu

--- menu.sh
+++ menu.sh
@@ -1,68 +1,55 @@
 #!/usr/bin/env bash
+source shared_function.sh
+
+setup() {
+    package_install dialog
+
+    ANSWER="/tmp/dot-scripts"
+    HIGHLIGHT=0
+}
 
 main_menu() {
-
-    if [[ $HIGHLIGHT != 9 ]]; then
+    if [ $HIGHLIGHT != 12 ]; then
         HIGHLIGHT=$(( HIGHLIGHT + 1 ))
     fi
 
-    dialog --default-item ${HIGHLIGHT} --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " $_MMTitle " \
-    --menu "$_MMBody" 0 0 9 \
-    "1" "$_PrepMenuTitle" \
-    "2" "$_InstBsMenuTitle" \
-    "3" "$_ConfBseMenuTitle" \
-    "4" "$_InstGrMenuTitle" \
-    "5" "$_InstNMMenuTitle" \
-    "6" "$_InstMultMenuTitle" \
-    "7" "$_SecMenuTitle" \
-    "8" "$_SeeConfOptTitle" \
-    "9" "$_Done" 2>${ANSWER}
-
-    HIGHLIGHT=$(cat ${ANSWER})
-
-    # Depending on the answer, first check whether partition(s) are mounted and whether base has been installed
-    if [[ $(cat ${ANSWER}) -eq 2 ]]; then
-        check_mount
-    fi
-
-    if [[ $(cat ${ANSWER}) -ge 3 ]] && [[ $(cat ${ANSWER}) -le 8 ]]; then
-        check_mount
-        check_base
-    fi
+    dialog --default-item ${HIGHLIGHT} \
+        --menu "Dot-scripts" 0 0 12 \
+        "1"  "Set locales" \
+        "2"  "Configure mirrorlist" \
+        "3"  "Create new user" \
+        "4"  "Install AUR helper" \
+        "5"  "Install Xorg" \
+        "6"  "Install fonts" \
+        "7"  "Install desktop" \
+        "8"  "Install networking" \
+        "9"  "Install multimedia" \
+        "10" "Install CUPS" \
+        "11" "Install general software" \
+        "12" "Install development software" \
+    2>${ANSWER}
 
     case $(cat ${ANSWER}) in
-        "1") prep_menu
-        ;;
-        "2") install_base_menu
-        ;;
-        "3") config_base_menu
-        ;;
-        "4") install_graphics_menu
-        ;;
-        "5") install_network_menu
-        ;;
-        "6") install_multimedia_menu
-        ;;
-        "7") security_menu
-        ;;
-        "8") edit_configs
-        ;;
-        *) dialog --backtitle "$VERSION - $SYSTEM ($ARCHI)" --yesno "$_CloseInstBody" 0 0
-
-        if [[ $? -eq 0 ]]; then
-            umount_partitions
-            clear
-            exit 0
-        else
-            main_menu
-        fi
-
-        ;;
+        "1") sh set-locales.sh ;;
+        "2") sh configure-mirrorlist.sh ;;
+        "3") sh create-new-user.sh ;;
+        "4") sh install-aur-helper.sh ;;
+        "5") sh install-xorg.sh ;;
+        "6") sh install-fonts.sh ;;
+        "7") sh install-desktop.sh ;;
+        "8") sh install-networking.sh ;;
+        "9") sh install-multimedia.sh ;;
+        "10") sh install-cups.sh ;;
+        "11") sh install-general-software.sh ;;
+        "12") sh install-development-software.sh ;;
+        *) clear; echo "Thank you for using Dot-scripts"; exit ;;
     esac
 
+    HIGHLIGHT=$(cat ${ANSWER})
+    main_menu
 }
 
+
 # Execution
-while true; do
-    main_menu
-done
+setup
+main_menu
